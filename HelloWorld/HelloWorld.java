42cab83a6b6542992b4cdb10977eb38989ad9207
root <root@DESKTOP-V882PTR.localdomain>
2019-10-02 13:35:39 +0900

    첫 커밋

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+class HelloWorld {
+	//Entry point
+	public static void main(String[] args) {
+	  System.out.println("Hello world!");
+	  int a = 10;
+	  int b = a++;
+	  System.out.println("b = a++");
+	  System.out.println("a = " + a);
+	  System.out.println("b = " + b);
+	  System.out.println("");
+
+	  a = 10;
+	  b = ++a;
+	  System.out.println("b = ++a");
+	  System.out.println("a = " + a);
+	  System.out.println("b = " + b);
+	  System.out.println("");
+	}
+}
---
+++ RomanToNumber/Ascll.java
@@ -0,0 +1,6 @@
+class Ascll {
+    public static void main(String[] args) {
+    char a = '\u0032';
+    System.out.println(a);
+    }
+}
